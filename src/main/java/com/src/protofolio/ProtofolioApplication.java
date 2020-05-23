c36df13e6be692af234d3103ca4094c46e83db54
Kalyan <kalyankumar0553@gmail.com>
2020-05-23 20:44:42 +0530

    Converted to spring boot

---
+++ src/main/java/com/src/protofolio/ProtofolioApplication.java
@@ -0,0 +1,13 @@
+package com.src.protofolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtofolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtofolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/src/protofolio/ProtofolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.src.protofolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtofolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
